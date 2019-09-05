d21d18eaef966d7f6d9cf3c311d9e01fe5eb7eca
PatrykPetlak <patryk.petlak@gmail.com>
2019-09-05 19:22:00 +0200

    aktualizacja

--- zadanie 1/index.js
+++ zadanie 1/index.js
@@ -23,6 +23,37 @@ for(let i = 0; i < numberRows; i++) {
     }
     numbers.push(row)
 }
+functfunction getColor() {
+  return (
+    "#" +
+    Math.random()
+      .toString(16)
+      .slice(2, 8)
+  );
+}io
+console.log(getColor(
+    
+    
+    n getColor() {
+    return (
+      "#" +
+      Math.random()
+        .toString(16)
+        .slice(2, 8)
+    );
+  }
+/*
+[
+    {
+        number: 12,
+        color: "#122122"
+    },
+    {
+        number: 1,
+        color: "#121adfd"
+    }
+]
+*/
 
 // let num = [
 //     [1,2],
@@ -37,21 +68,76 @@ for(let i = 0; i < numberRows; i++) {
 //     }
 // }
 
+const group = []
+group.push(12)
+
 for(let i = 0; i < numbers.length; i++) {
     let row = document.createElement("tr")
     for(let k = 0; k < numbers[i].length; k++) {
        let cell = document.createElement("td")
+       if(numbers[i][k] % 2 === 0) {
+            cell.style.color = "orange"
+       } else {
+           cell.style.color = "lightblue"
+       }
        cell.innerText = numbers[i][k]
        row.appendChild(cell)
     }
     document.querySelector("table").appendChild(row)
 }
 
+let obj = {
+    a: "asd",
+    b:" testst",
+    fan: function() {
+        console.log("fan")
+    }
+}
+obj.a = true
+console.log(obj.a)
+obj.fan()
+
+// let person = {
+//     name: "Marcin",
+//     surname: "Warzybok"
+// }
+// let person2 = {
+//     name: "Jan",
+//     surname: "Kowalksi"
+// }
+
 // let num = [
-//     [1,2,10,12],
-//     [2,5,10,12],
+//     [1,2,10,1],
+//     [2,2,10,12],
 //     [1,0,12,12]
 // ]
+
+/*
+
+let wystepowania = [
+    {
+        value: 1,
+        color: "#123123"
+    },
+    {
+        value: 2,
+        color: "#123F23"
+    },
+    {
+        value: 10,
+        color: "#12AD23"
+    },
+    {
+        value: 12,
+        color: "#123123"
+    },
+    {
+        value: 0,
+        color: "yellow"
+    }
+]
+
+*/
 
 console.log(numbers)
 
@@ -75,6 +161,8 @@ function minAndMax(array) {
     return {minArray, maxArray}
 }
 
+console.log(minAndMax(numbers))
+
 // function abc() {
 //    console.log("abccc")
 //    return 1
@@ -89,5 +177,32 @@ function minAndMax(array) {
 // let t = abc()
 // console.log(t)
 
-let ar = minAndMax(numbers)
-console.log(ar)
+// [
+//     [1,2,3],
+//     [2,4,5],
+//     [2,3,5]
+// ]
+//number[2][2]
+
+let max = null
+let min = null
+
+for(let i = 0; i < numbers.length; i++) {
+    let suma = 0
+    for(let k = 0; k < numbers[i].length; k++ ){
+        suma += numbers[i][k]
+    }
+    console.log(suma)
+    if(!max) {
+        max = suma
+    } else if(suma > max) {
+        max = suma
+    }
+    if(!min) {
+        min = suma
+    } else if(suma < min) {
+        min = suma
+    }
+}
+
+console.log(min, max)
